7e0f7145ff657ce9f17ef5781a9ae4e0ac1e2f30
Kus Cámara <kcmr@users.noreply.github.com>
2018-12-31 01:22:19 +0100

    another try…

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
+  git config --global user.email "${GH_EMAIL}"
+  git config --global user.name "${GH_NAME}"
+
+  git clone "${CIRCLE_REPOSITORY_URL}" out
+
+  cd out
+  git checkout "${TARGET_BRANCH}" || git checkout --orphan "${TARGET_BRANCH}"
+  git rm -rf .
+  cd ..
+
+  npm run build
+
+  cp -a build/es6-bundled/demo/. out/.
+
+  cd out
+
+  git add -A
+  git commit -m '[ci skip] deploy to Github Pages' --allow-empty
+  git push origin "${TARGET_BRANCH}"
+fi
